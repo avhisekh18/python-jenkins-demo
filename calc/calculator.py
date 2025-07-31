f36b8d8f709e5d14bef8ea3e1ce744f7aa462859
Abhi D <avhisekhdhungana@gmail.com>
2025-07-31 17:35:41 +0545

    A little change in calculator.py

--- calc/calculator.py
+++ calc/calculator.py
@@ -12,23 +12,25 @@ def divide(a , b):
         raise ValueError("cannot divide by 0")
     return a/b
 
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+if __name__ == '__main__':
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    
+    choice = input("Enter choice (1/2/3/4): ")
 
-choice = input("Enter choice (1/2/3/4): ")
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
-elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
-elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
-elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Invalid input")   
+    if choice == '1':
+        print(f"The result is: {add(num1, num2)}")
+    elif choice == '2':
+        print(f"The result is: {subtract(num1, num2)}")
+    elif choice == '3':
+        print(f"The result is: {multiply(num1, num2)}")
+    elif choice == '4':
+        print(f"The result is: {divide(num1, num2)}")
+    else:
+        print("Invalid input")
